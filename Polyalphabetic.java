3d529a545b0642f1c21e51e2620f996f4163f605
Bhasha Dushara <42309082+Bhasha4995dusara@users.noreply.github.com>
2018-11-18 19:03:30 +0530

    Create Polyalphabetic.java
    AIM: IMPLEMENT POLY ALPHABETIC CIPHER (VIGENERE CIPHER).

---
+++ Polyalphabetic.java
@@ -0,0 +1,46 @@
+package polyalphabetic;
+import java.util.*;
+ public class Polyalphabetic
+{
+public static String encrypt(String text, final String key)
+    {
+        String res = "";
+        text = text.toUpperCase();
+        for (int i = 0, j = 0; i < text.length(); i++)
+        {
+            char c = text.charAt(i);
+            if (c < 'A' || c > 'Z')
+                continue;
+            res += (char) ((c + key.charAt(j) - 2 * 'A') % 26 + 'A');
+            j = ++j % key.length();
+        }
+        return res;
+    }
+ 
+    public static String decrypt(String text, final String key)
+    {
+        String res = "";
+        text = text.toUpperCase();
+        for (int i = 0, j = 0; i < text.length(); i++)
+        {
+            char c = text.charAt(i);
+            if (c < 'A' || c > 'Z')
+                continue;
+            res += (char) ((c - key.charAt(j) + 26) % 26 + 'A');
+            j = ++j % key.length();
+        }
+        return res;
+    }
+ public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("please enter message:");
+        String message = sc.nextLine();
+        System.out.println("please enter key");
+        String key = sc.next();
+        String encryptedMsg = encrypt(message, key);
+        System.out.println("String: " + message);
+        System.out.println("Encrypted message: " + encryptedMsg);
+        System.out.println("Decrypted message: " + decrypt(encryptedMsg, key));
+    } 
+}
